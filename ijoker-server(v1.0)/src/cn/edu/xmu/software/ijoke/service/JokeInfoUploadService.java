dfa50eaea7e3a69f4c618220f2d278543f521ab2
BillWhiteBee <BillWhiteBee@72b28f28-660a-87ad-c753-09ae21b4e747>
2010-05-17 17:25:52 +0000

    关于上传

--- ijoker-server(v1.0)/src/cn/edu/xmu/software/ijoke/service/JokeInfoUploadService.java
+++ ijoker-server(v1.0)/src/cn/edu/xmu/software/ijoke/service/JokeInfoUploadService.java
@@ -7,9 +7,8 @@ public interface JokeInfoUploadService {
 	public void jokeInfoUploadService (String title, String keyWord, 
 			String userId,String fileExtension,String fileName);
 	
-	public String jokeInfoUploadServiceByServer (
-			String title, String keyWord, 
-			String userId,String fileExtension,String fileId);
+
 	
 	public boolean jokeInfoUploadServiceByServer(String title,String key,String userId,File file);
+	
 }
